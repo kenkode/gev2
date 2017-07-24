9ac0b61519da479de867a8a1dc0a4d6d27a67ac6
Oirere-Jr <eoirere8@gmail.com>
2017-07-24 17:48:25 +0300

    Added login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+Route::get('/register', function() {
+    return view('register');
+})->name('register');
+
+Route::get('/login', function() {
+    return view('login');
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
